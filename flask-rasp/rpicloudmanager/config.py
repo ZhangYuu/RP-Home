34528cf87eadf208b33d534cc265be18d5f58062
ZhangYuu <memo40k@outlook.com>
2017-10-20 22:04:39 -0400

    Add files via upload

---
+++ flask-rasp/rpicloudmanager/config.py
@@ -0,0 +1,16 @@
+import os
+class Config(object):
+  BASEDIR = os.path.abspath(os.path.dirname(__file__))
+  DATABASE = BASEDIR+'/db/rpi.db'
+  SECRET_KEY = os.urandom(24)
+  USERNAME = 'admin'
+  PASSWORD = 'default'
+  DEBUG = True
+  HOST = '0.0.0.0'
+  PORT = 2000
+class Development(Config):
+  pass
+class Production(Config):
+  DEBUG = False
+  HOST = '0.0.0.0'
+  PORT = 8080
